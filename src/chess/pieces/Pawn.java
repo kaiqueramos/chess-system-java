e4beb45dbd2131147d121adbd4a5611e9d2eb0d9
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-09-13 01:02:11 -0300

    Bishop and some changes on Pawn

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -47,13 +47,13 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColumn()] = true;
             }
             p.setValues(position.getRow() + 2, position.getColumn());
-            Position p2 = new Position(p.getRow() + 1, p.getColumn());
+            Position p2 = new Position(p.getRow() - 1, p.getColumn()); //Possible error here??
             if (
-                    getBoard().positionExists(p)
-                            && !getBoard().thereIsAPiece(p)
-                            && getMoveCount() == 0
-                            && getBoard().positionExists(p2)
-                            && !getBoard().thereIsAPiece(p2)
+                getBoard().positionExists(p)
+                && !getBoard().thereIsAPiece(p)
+                && getMoveCount() == 0
+                && getBoard().positionExists(p2)
+                && !getBoard().thereIsAPiece(p2)
             ) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
@@ -62,9 +62,6 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColumn()] = true;
             }
             p.setValues(position.getRow() + 1, position.getColumn() + 1);
-        }
-        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-            mat[p.getRow()][p.getColumn()] = true;
         }
         return mat;
     }
